d502528362cf6be2a45763f8d0643f054b33033d
olegbc <drum70@mail.ru>
2016-05-23 04:18:26 +0300

    empty advanced yii2 project

---
+++ frontend/views/site/about.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About us';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Here you can get some information about us.</p>
+
+</div>
---
+++ frontend/views/site/invitations.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Invitations';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Here you can invite people, view own invitations, view invitations’ details.</p>
+
+</div>
